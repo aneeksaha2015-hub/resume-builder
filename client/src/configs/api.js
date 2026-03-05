12256394df581dd04330986bb78fba8a834ed029
aneeksaha2015-hub <aneeksaha2015@gmail.com>
2026-03-05 18:38:04 +0530

    Fix syntax and formatting in api.js

--- client/src/configs/api.js
+++ client/src/configs/api.js
@@ -1,9 +1,10 @@
-import axios from 'axios'
+import axios from 'axios';
 
-console.log("BASE URL IS:", import.meta.env.VITE_BASE_URL)
+console.log("BASE URL IS:", import.meta.env.VITE_BASE_URL);
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_BASE_URL
-})
+  baseURL: import.meta.env.VITE_BASE_URL, // e.g., https://resume-builder.onrender.com/api
+  // withCredentials: true, // only if you use cookies
+});
 
-export default api
+export default api;
