9e2aae93e17f2b76b7c82992ba4c58c3cc4cfe63
aneeksaha2015-hub <aneeksaha2015@gmail.com>
2026-03-05 17:22:42 +0530

    Update api.js

--- client/src/configs/api.js
+++ client/src/configs/api.js
@@ -1,9 +1,4 @@
-import axios from 'axios'
-
-console.log("BASE URL IS:", import.meta.env.VITE_BASE_URL)
-
-const api = axios.create({
-    baseURL: import.meta.env.VITE_BASE_URL
-})
-
+import axios from"axios"
+console.log("BASE URL IS:",import.meta.env.VITE_BASE_URL)
+const api=axios.create({baseURL:import.meta.env.VITE_BASE_URL})
 export default api
